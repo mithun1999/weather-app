fc21ea2dddbe0f7c0be42f48a196f5f940e4a3a2
mithun1999 <mithunkumari@karunya.edu.in>
2020-01-06 23:11:01 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Weather from './app_component/weather.component';
+import Form from './app_component/form.component';
+import Head from './app_component/heading.component';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//api.openweathermap.org/data/2.5/weather?q=London
+const API_key="4c4bca2473fe61c54522eec4504a55be";
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      city:undefined,
+      country:undefined,
+      icon:undefined,
+      main:undefined,
+      celsius:undefined,
+      temp_max:undefined,
+      temp_min:undefined,
+      description:"",
+      error:false
+    };
+
+    this.weatherIcon={
+      Thunderstorm:"wi-thunderstorm",
+      Drizzle:"wi-sleet",
+      Rain:"wi-storm-showers",
+      Snow:"wi-snow",
+      Atmosphere:"wi-fog",
+      Clear:"wi-day-sunny",
+      Clouds:"wi-day-fog"
+    };
+  }
+
+  calCelsius(temp){
+    let cel=Math.floor(temp-273.15);
+    return cel;
+  }
+
+  get_WeatherIcon(icons,rangeId){
+    switch(true){
+      case rangeId >= 200 && rangeId < 232:
+        this.setState({ icon: icons.Thunderstorm });
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: icons.Drizzle });
+        break;
+      case rangeId >= 500 && rangeId <= 521:
+        this.setState({ icon: icons.Rain });
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: icons.Snow });
+        break;
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: icons.Atmosphere });
+        break;
+      case rangeId === 800:
+        this.setState({ icon: icons.Clear });
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: icons.Clouds });
+        break;
+      default:
+        this.setState({ icon: icons.Clouds });
+    }
+  }
+
+  getWeather=async(e)=>{
+
+    e.preventDefault();
+
+    const city=e.target.elements.city.value;
+    const country=e.target.elements.country.value;
+
+    if(city && country){
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`
+      );
+      const response = await api_call.json();
+      console.log(response);
+      this.setState({
+        city:`${response.name},${response.sys.country}`,
+        celsius:this.calCelsius(response.main.temp),
+        temp_max:this.calCelsius(response.main.temp_max),
+        temp_min:this.calCelsius(response.main.temp_min),
+        description:response.weather[0].description,
+        error:false
+      });
+  
+      this.get_WeatherIcon(this.weatherIcon,response.weather[0].id);
+    }
+    else{
+      this.setState({error:true});
+    }
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <Head/>
+        <Form loadweather={this.getWeather} error={this.state.error} />
+        <Weather 
+        city={this.state.city} 
+        country={this.state.country}
+        temp_celsius={this.state.celsius}
+        temp_max={this.state.temp_max}
+        temp_min={this.state.temp_min}
+        description={this.state.description}
+        weatherIcon={this.state.icon}
+        />
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/app_component/heading.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './heading.style.css';
+
+const Head = () =>{
+    return(
+        <h1 className="title py-5">Mithun's Weather App</h1>
+    );
+}
+
+export default Head;

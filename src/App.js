d53f274a5e49911fe80538cfe77d6215e9e9d6b9
mithun1999 <mithunkumari@karunya.edu.in>
2020-01-06 23:27:50 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component{
 
     if(city && country){
       const api_call = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`
       );
       const response = await api_call.json();
       console.log(response);
